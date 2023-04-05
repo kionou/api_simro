8490e067b2ccab855590f005a96825a948828d73
kionou <kionoumamadou.00@gmail.com>
2023-04-05 23:27:47 +0000

    ok

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var marketRouter = require('./routes/market');
+
 
 var app = express();
 
@@ -15,12 +18,15 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/market', marketRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
