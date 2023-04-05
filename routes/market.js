ad273b8418dba3aeac5cfd7c479fc573ea73416a
kionou <kionoumamadou.00@gmail.com>
2023-04-05 23:38:36 +0000

    ok

--- routes/market.js
+++ routes/market.js
@@ -1,9 +1,17 @@
+const axios  = require('axios');
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.json("bonjour")
+    axios
+    .get('http://simro-kberthe.pythonanywhere.com/api/marche/')
+    .then((resul) =>{
+      console.log("ee",resul.data);
+      res.json(resul.data)
+    })
+
 });
 
 module.exports = router;
