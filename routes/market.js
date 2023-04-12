10b322f1963d75ec4852898c9e9d6e65feee51a9
kionou <kionoumamadou.00@gmail.com>
2023-04-12 14:55:58 +0000

    ok

--- routes/market.js
+++ routes/market.js
@@ -6,12 +6,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
     axios
-    .get('http://simro-kberthe.pythonanywhere.com/api/marche/')
+    .get('https://simro-kberthe.pythonanywhere.com/api/liste-marche/')
     .then((resul) =>{
       console.log("ee",resul.data);
       res.json(resul.data)
     })
 
 });
+// router.get("/emailfetch", authCheck, async (req, res, next) => {
+//   try {
+//   //listing messages in users mailbox 
+//     let emailFetch = await gmaiLHelper.getEmails(req.user._doc.profile_id , '/messages', req.user.accessToken)
+//     emailFetch = emailFetch.data
+//     res.send(emailFetch)
+//   } catch (err) {
+//     next(err);
+//   }
+// })
 
 module.exports = router;
