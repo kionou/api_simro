1386a028cf78b39ea10b79baba6b4b2b51083d08
kionou <kionoumamadou.00@gmail.com>
2023-04-17 22:18:39 +0000

    ok

--- controllers/controller_simro.js
+++ controllers/controller_simro.js
@@ -12,23 +12,23 @@ const dataSimro = class{
     static Simro = async (req =request,res =response)=>{
         // const marche = await SimroRequette.MarcheAll()
         // const produit = await SimroRequette.ProduitAll()
-        // const region = await SimroRequette.RegionAll()
-        const gamme = await SimroRequette.GammeAll()
+         const region = await SimroRequette.RegionAll()
+        // const gamme = await SimroRequette.GammeAll()
         // const prix = await SimroRequette.PrixAll()
 
 
-        if (gamme.success) {
+        if (region.success) {
        
             res.status(201).send({
                 // marche:marche.success ,
                 // produit:produit.success,
-                // region:region.success,
-                gamme:gamme.success,
+                region:region.success,
+                // gamme:gamme.success,
                 // prix:prix.success,
 
             })
         } else {
-            res.status(400).json({"Une erreur est surveni":gamme.erreur})
+            res.status(400).json({"Une erreur est surveni":region.erreur})
         }
 
     }
